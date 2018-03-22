8613fc2d1b42a01b5db45c1dbd67b73a330b95a2
ruslanvs <ruslanvs@yahoo.com>
2018-03-22 15:32:15 -0700

    Blank

---
+++ curve_ballsUITests/curve_ballsUITests.swift
@@ -0,0 +1,36 @@
+//
+//  curve_ballsUITests.swift
+//  curve_ballsUITests
+//
+//  Created by Ruslan Suvorov on 3/22/18.
+//  Copyright © 2018 Ruslan Suvorov. All rights reserved.
+//
+
+import XCTest
+
+class curve_ballsUITests: XCTestCase {
+        
+    override func setUp() {
+        super.setUp()
+        
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+}
